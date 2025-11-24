5915ec1f382515c43ee402c49a7fa78816b9be6f
deya19 <deyasaleh2@gmail.com>
2025-11-24 22:39:30 +0300

    implement api routing, server actions, cashing and design the Event Details page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,38 @@
-import EventCard from '@/components/EventCard'
-import ExploreBtn from '@/components/ExploreBtn'
-import events from '@/constants'
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/ExploreBtn";
+// import events from "@/constants";
+import {IEvent} from "@/database";
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-export default function page() {
+export default async function Page() {
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
+
   return (
     <section>
-            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
-            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can&apost Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
 
-            <ExploreBtn />
+      <ExploreBtn />
 
-            <div className="mt-20 space-y-7">
-                <h3>Featured Events</h3>
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
 
-                <ul className="events">
-                    {events && events.length > 0 && events.map((event: IEvent) => (
-                        <li key={event.title} className="list-none">
-                            <EventCard {...event} />
-                        </li>
-                    ))}
-                </ul> 
-            </div>
-        </section>
-  )
+        <ul className="events">
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title} className="list-none">
+                <EventCard {...event} />
+              </li>
+            ))}
+        </ul>
+      </div>
+    </section>
+  );
 }
